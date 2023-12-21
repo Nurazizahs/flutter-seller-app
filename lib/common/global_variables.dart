d70f1377f4f42aa48e5ff2993e44e853cb447d08
Nurazizahs <nurazizahsabilillah@gmail.com>
2023-12-22 01:46:02 +0700

    upload image, add product

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,4 +1,4 @@
 class GlobalVariables {
   // static const baseUrl = 'https://flutterdev.my.id';
-  static const baseUrl = 'https://f9c8-43-252-106-28.ngrok.io';
+  static const baseUrl = 'https://3ecb-43-252-106-28.ngrok-free.app';
 }
